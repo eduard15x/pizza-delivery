892c00db712c61f540b7a183c6eca45b0d4d4a84
Precup Eduard <precupeduard99@gmail.com>
2023-05-25 18:15:08 +0300

    Update - Admin Panel
    Finish the updates for all request types
    A little clean code

--- client/src/pages/AdminPanel.js
+++ client/src/pages/AdminPanel.js
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import NavBar from "../components/NavBar";
 const allProductsURL = process.env.REACT_APP_ALL_PRODUCTS;
 const deleteProductURL = process.env.REACT_APP_DELETE_PRODUCT;
+const updateProductURL = process.env.REACT_APP_DELETE_PRODUCT;
 
 const AdminPanel = () => {
   // get products
   const [productsList, setProductsList] = useState([]);
   // productObj
   const [newProduct, setNewProduct] = useState({});
+  // updateProduct modal
+  const [showModal, setShowModal] = useState(false);
 
   // this can be created as a customHook
   async function getProducts() {
@@ -33,12 +37,9 @@ const AdminPanel = () => {
     e.preventDefault();
     try {
       const response = await axios.post(allProductsURL, newProduct);
-      console.log(newProduct);
       console.log("Response:", response.data);
-      // Handle the response as needed
     } catch (error) {
       console.error("Error:", error);
-      // Handle the error as needed
     }
   }
 
@@ -48,22 +49,44 @@ const AdminPanel = () => {
       const response = await axios.delete(deleteProductURL + id);
       console.log("Response:", response.data);
       getProducts();
-      // Handle the response as needed
     } catch (error) {
       console.error("Error:", error);
-      // Handle the error as needed
     }
   }
 
   console.log(newProduct);
+
+  function handleUpdate(e) {
+    setShowModal(true);
+    const productID = e.target.dataset.productId;
+    const { _id, name, description, price, stockLevel, images } =
+      productsList.find((item) => item._id === productID);
+    setNewProduct({
+      _id,
+      name,
+      description,
+      price,
+      stockLevel,
+      images,
+    });
+  }
+
+  async function handleSubmitUpdate(id) {
+    try {
+      const response = await axios.patch(updateProductURL + id, newProduct);
+      console.log("Response:", response.data);
+      getProducts();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
   useEffect(() => {
     getProducts();
   }, []);
 
   return (
-    <div>
-      <Link to={"/contact"}>Go</Link>
+    <div className="admin-panel">
       <p>Hello Admin</p>
       <p>This is admin panel</p>
       <div>
@@ -74,13 +97,74 @@ const AdminPanel = () => {
               <li key={item._id}>
                 <p>{item.name}</p>
                 <p onClick={() => handleDelete(item._id)}>DeleteBTN ICON</p>
-                <p data-product-id={item._id}>Modify ICON</p>
+                <p data-product-id={item._id} onClick={handleUpdate}>
+                  Modify ICON
+                </p>
               </li>
             ))}
           </ul>
         ) : (
           ""
         )}
+      </div>
+
+      <div className={`${showModal ? "" : "d-none"}`}>
+        <button onClick={() => setShowModal(false)}>Close</button>
+        <p>Title of the modal</p>
+        <form
+          onSubmit={() => {
+            handleSubmitUpdate(newProduct._id);
+          }}
+        >
+          <input
+            required
+            type="text"
+            name="name"
+            placeholder="Product Name"
+            defaultValue={newProduct.name}
+            onChange={(e) => updateNewProduct("name", e.target.value)}
+          />
+          <textarea
+            required
+            name="description"
+            placeholder="Product Description"
+            defaultValue={newProduct.description}
+            onChange={(e) => updateNewProduct("description", e.target.value)}
+          ></textarea>
+          <input
+            required
+            type="number"
+            name="price"
+            placeholder="Product Price"
+            defaultValue={newProduct.price}
+            onChange={(e) => updateNewProduct("price", e.target.value)}
+          />
+          <input
+            required
+            type="number"
+            name="stockLevel"
+            placeholder="Product Stock"
+            defaultValue={newProduct.stockLevel}
+            onChange={(e) => updateNewProduct("stockLevel", e.target.value)}
+          />
+          <input
+            required
+            type="file"
+            name="images"
+            placeholder="Product Image"
+            defaultValue=""
+            onChange={(e) =>
+              updateNewProduct("images", [
+                {
+                  base64: "imageSRC",
+                  name: "imageNAME",
+                  lastModified: 3131,
+                },
+              ])
+            }
+          />
+          <input type="submit" value="Save" />
+        </form>
       </div>
 
       <div>
